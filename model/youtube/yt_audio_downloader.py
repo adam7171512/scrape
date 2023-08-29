9b1ea7332ca2bf22583d66f4d6068577478ba326
adam7 <adam7171512@gmail.com>
2023-08-29 13:09:29 +0200

    add docstring and underscore private variables

--- model/youtube/yt_audio_downloader.py
+++ model/youtube/yt_audio_downloader.py
@@ -6,6 +6,17 @@ from model.youtube.core import YtVideo
 
 
 class YtAudioDownloader:
+    """
+    A class to handle downloading of YouTube videos as audio files.
+    Initializer accepts optional length_min parameter, to truncate the length of audio files
+
+    Example:
+        downloader = YtAudioDownloader(length_min=5)
+        path = downloader.download(YtVideo("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+
+    Dependencies:
+        yt_dlp, ffmpeg
+    """
     # download audio only
     params = {
         "format": "bestaudio/best",
@@ -20,26 +31,26 @@ class YtAudioDownloader:
     }
 
     def __init__(self, length_min: int = None):
-        self.length_min = length_min
-        self.ytdl = yt.YoutubeDL(self.params)
-        self.downloaded_files = []
+        self._length_min = length_min
+        self._ytdl = yt.YoutubeDL(self.params)
+        self._downloaded_files = []
 
     # returns path to downloaded file
     def download(self, video: YtVideo) -> str:
         url = video.url
         print(f"downloading {url}")
-        self.ytdl.download([url])
+        self._ytdl.download([url])
 
         # if short, then cut the audio to first 5 minutes
-        if self.length_min:
-            l_min = str(self.length_min).zfill(2)
+        if self._length_min:
+            l_min = str(self._length_min).zfill(2)
             os.system(
                 f"ffmpeg -i ./{video.video_id}.wav -ss 00:00:00 -to 00:{l_min}:00 -c copy ./{video.video_id}_short.wav"
             )
             os.remove(f"./{video.video_id}.wav")
             os.rename(f"./{video.video_id}_short.wav", f"./{video.video_id}.wav")
 
-        self.downloaded_files.append(f"{video.video_id}.wav")
+        self._downloaded_files.append(f"{video.video_id}.wav")
 
         # full path :
         path = f"{os.getcwd()}/{video.video_id}.wav"
